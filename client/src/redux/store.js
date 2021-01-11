d8c5ac1c9ededa4f397475cb9206e0fc2bb5839c
Vitalii Petrushka <vpetrushka52@gmail.com>
2021-01-11 10:36:46 +0200

    fixed redux devtools issue

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,9 +2,12 @@ import { applyMiddleware, createStore, compose } from "redux";
 import thunk from "redux-thunk";
 import reducers from "./reducers";
 
-const enhanced = compose(
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+// ONLY in dev purposes,
+// potential bug if extensions not installed in browser
+//
+// const enhanced = compose(
+//   applyMiddleware(thunk),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
 
-export default createStore(reducers, enhanced);
+export default createStore(reducers, applyMiddleware(thunk));
